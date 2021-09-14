a1c30fd5eaa4f05f70d3ec79363a2345547ae32a
Andrii Kyreikov <Andropolog@gmail.com>
2021-09-14 12:17:29 +0300

    done

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -4,7 +4,7 @@ import styles from "./Views.Module.css";
 const HomeView = () => (
   <div className={styles.h}>
     <h1>
-      Добро пожаловать <br/> на сайт <p className={styles.h1}>PHONEBOOK.APP</p>
+      Добро пожаловать <br/> на сайт <p className={styles.h1}>PHONEBOOK</p>
     </h1>
   </div>
 );
